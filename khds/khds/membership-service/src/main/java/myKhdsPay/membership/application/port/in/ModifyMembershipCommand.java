df80e413352d45f7919ef1546829a7594d294767
khdscor <khds_cor@naver.com>
2024-01-11 23:44:47 +0900

    feat: 회원 수정 in-port 구현

---
+++ khds/khds/membership-service/src/main/java/myKhdsPay/membership/application/port/in/ModifyMembershipCommand.java
@@ -0,0 +1,46 @@
+package myKhdsPay.membership.application.port.in;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import myKhdsPay.common.SelfValidating;
+
+
+@Builder
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class ModifyMembershipCommand extends SelfValidating<ModifyMembershipCommand> {
+
+    @NotNull
+    private final String membershipId;
+
+    @NotNull
+    private final String name;
+
+    @NotNull
+    private final String email;
+
+    @NotNull
+    @NotBlank
+    private final String address;
+
+    @AssertTrue
+    private final boolean isValid;
+
+    private final boolean isCorp;
+
+    public ModifyMembershipCommand(String membershipId, String name, String email, String address, boolean isValid,
+                                   boolean isCorp) {
+        this.membershipId = membershipId;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.isValid = isValid;
+        this.isCorp = isCorp;
+
+        this.validateSelf();
+    }
+}
