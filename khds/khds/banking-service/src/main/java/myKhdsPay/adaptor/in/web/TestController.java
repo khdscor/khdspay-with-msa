1269ea9547888f123f6f126c2c16ca074bf7ff84
Khdscor <khds_cor@naver.com>
2024-01-14 18:56:22 +0900

    feat: banking-service h2 연동 및 테스트

---
+++ khds/khds/banking-service/src/main/java/myKhdsPay/adaptor/in/web/TestController.java
@@ -0,0 +1,13 @@
+package myKhdsPay.adaptor.in.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "gggggggg";
+    }
+}
