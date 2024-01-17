1d71cd6e286376948108b719cf68110f2c5c89f1
khdscor <khds_cor@naver.com>
2024-01-17 22:46:55 +0900

    feat: 은행간 돈 이동을 위한 in-adaptor 구현

---
+++ khds/khds/banking-service/src/main/java/myKhdsPay/adaptor/in/web/RequestFirmBankingRequest.java
@@ -0,0 +1,18 @@
+package myKhdsPay.adaptor.in.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestFirmBankingRequest {
+
+    //  -> b 실물 계좌로 요청을 하기 위한 Request
+    private String fromBankName;
+    private String fromBankAccountNumber;
+    private String toBankName;
+    private String toBankAccountNumber;
+    private int moneyAmount;
+}
