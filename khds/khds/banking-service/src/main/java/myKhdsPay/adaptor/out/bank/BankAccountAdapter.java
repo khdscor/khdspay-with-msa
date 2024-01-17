09375dbce5be7700d22bd6a54bd8b4e1c9b79c46
khdscor <khds_cor@naver.com>
2024-01-17 22:48:45 +0900

    feat: 은행간 돈 이동을 위한 외부 은행 호출 out-adaptor 구현

--- khds/khds/banking-service/src/main/java/myKhdsPay/adaptor/out/bank/BankAccountAdapter.java
+++ khds/khds/banking-service/src/main/java/myKhdsPay/adaptor/out/bank/BankAccountAdapter.java
@@ -2,12 +2,13 @@ package myKhdsPay.adaptor.out.bank;
 
 import lombok.RequiredArgsConstructor;
 import myKhdsPay.application.port.out.RequestBankAccountInfoPort;
+import myKhdsPay.application.port.out.RequestExternalFirmBankingPort;
 import myKhdsPay.common.ExternalSystemAdapter;
 
 
 @ExternalSystemAdapter
 @RequiredArgsConstructor
-public class BankAccountAdapter implements RequestBankAccountInfoPort {
+public class BankAccountAdapter implements RequestBankAccountInfoPort, RequestExternalFirmBankingPort {
     @Override
     public BankAccount getBankAccountInfo(GetBankAccountRequest request) {
 
@@ -17,5 +18,17 @@ public class BankAccountAdapter implements RequestBankAccountInfoPort {
         //실제 은행 계좌 -> BankAccount
 
         return new BankAccount(request.getBankName(), request.getBankAccountNumber(), true);
+    }
+
+    @Override
+    public FirmBankingResult requestExternalFirmBanking(ExternalFirmBankingRequest request) {
+
+        //실제로 외부 은행에 http 통신을 통해서
+        //펌뱅킹 요청을 하고
+
+        //그 결과를
+        //외부 은행의 실제 결과를 -> 패캠 페이으 ㅣFirmBankingResult 작성
+        FirmBankingResult result = new FirmBankingResult(1);
+        return result;
     }
 }
